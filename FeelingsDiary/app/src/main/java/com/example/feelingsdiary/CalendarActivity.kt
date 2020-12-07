877c690d11f08525468e4dd43c3892b638e62312
camilledj27 <74025227+camilledj27@users.noreply.github.com>
2020-12-07 15:00:46 -0500

    Update CalendarActivity.kt

--- FeelingsDiary/app/src/main/java/com/example/feelingsdiary/CalendarActivity.kt
+++ FeelingsDiary/app/src/main/java/com/example/feelingsdiary/CalendarActivity.kt
@@ -15,31 +15,33 @@ import java.util.*
 class CalendarActivity : AppCompatActivity() {
 
     var calender: CalendarView? = null
-    var date_view: TextView? = null
+    var date: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Setting Calendar as the view
         setContentView(R.layout.activity_calendar)
 
         calender = findViewById<View>(R.id.calender) as CalendarView
-        date_view = findViewById<View>(R.id.date_view) as TextView
+        date = findViewById<View>(R.id.date_view) as TextView
 
-        // Source: http://developer.android.com/training/improving-layouts/smooth-scrolling.html
+        // Source: https://www.geeksforgeeks.org/android-creating-a-calendar-view-app/
         calender!!.setOnDateChangeListener(
             OnDateChangeListener { view, year, month, dayOfMonth ->
                 val m = month + 1;
                 val date = ( dayOfMonth.toString() + "-" + m + "-" + year)
-                date_view!!.text = date
+                date!!.text = date
             })
     }
 
     // source: https://www.tutorialspoint.com/how-to-add-calendar-events-in-android-app
+    // Adding events to the calendar using Google Calendar
     fun AddCalendarEvent(view: View?) {
-        val calendarEvent = Calendar.getInstance()
+        val event = Calendar.getInstance()
         val i = Intent(Intent.ACTION_EDIT)
         i.type = "vnd.android.cursor.item/event"
-        i.putExtra("startTime", calendarEvent.timeInMillis)
-        i.putExtra("endTime", calendarEvent.timeInMillis + 60 * 60 * 1000)
+        i.putExtra("startTime", event.timeInMillis)
+        i.putExtra("endTime", event.timeInMillis + 60 * 60 * 1000)
         i.putExtra("eventName", "Calendar Event")
         startActivity(i)
     }
